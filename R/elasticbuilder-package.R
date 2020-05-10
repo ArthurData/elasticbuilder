d949d889a2be68b50c125e71801ee94b05c5676c
ArthurData <arthur@critizr.com>
2020-05-10 19:24:24 +0200

    Start documenting package

---
+++ R/elasticbuilder-package.R
@@ -0,0 +1,9 @@
+#' elasticbuilder: Helper to Elasticsearch queries.
+#'
+#' This package is an helper to build your Elasticsearch queries.
+#'
+#' More details will come !
+#'
+#' @docType package
+#' @name elasticbuilder
+NULL
